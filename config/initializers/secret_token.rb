2f031d3c6513362dd0959a14be95aca326389ec8
Dan Seely <dan@danseely.net>
2014-01-20 20:26:37 -0500

    first local commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imperialblaster::Application.config.secret_token = 'd71bb63e1ea04593d52b8fb01529e130715270b5a5fbe67c4e403dfdae26d574326e838fb8bb8a9b5f44d551f520d93eaa39c9857f520e0e3be68ec23740352b'
