477db717a65c54d43761a569b60b4ade3b0d258a
Dan Seely <dan@danseely.net>
2014-06-01 16:13:12 -0400

    pulled in known-working user system from magpie app. Uses sorcery gem for auth. need to test.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
-  attr_accessible :email, :password, :password_confirmation, :username, :name
+  attr_accessible :email, :password, :password_confirmation, :name
 
   validates :name,      :presence => true
   validates :email,     :presence => true,
                         :uniqueness => true,
                         :format => { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/  }
-  validates :username,  :presence => true,
-                        :uniqueness => true
   validates :password,  :presence => true, if: :password,
                         :confirmation => true, if: :password
 end
